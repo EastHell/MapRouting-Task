3af9511c97abcc4c3bedcbd14f277f5e3ed4ffb9
EastHell <djturbo9393@gmail.com>
2021-04-05 22:27:13 +0300

    retarget to iOS 12+ and map added

--- MapRouting-Task/ViewController.swift
+++ MapRouting-Task/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
-
-
 }
 
